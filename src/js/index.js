121a3c064f93dd220ffdf33a39a200b79eacf4d9
Paulo Silva <paulofernando78@gmail.com>
2025-11-02 06:00:26 -0300

    wip

--- src/js/index.js
+++ src/js/index.js
@@ -12,11 +12,11 @@ navLinks.forEach((link) => {
   });
 });
 
-const darkModeBtn = document.querySelector("wc-button[icon='darkMode']");
+const darkModeBtn = document.querySelector("wc-button[icon='lightMode']");
 
 darkModeBtn.addEventListener("click", () => {
   const current = document.body.getAttribute("data-theme");
   const newTheme = current === "dark" ? "light" : "dark";
   document.body.setAttribute("data-theme", newTheme);
-  darkModeBtn.setAttribute("icon", newTheme === "dark" ? "lightMode" : "darkMode")
+  darkModeBtn.setAttribute("icon", newTheme === "dark" ? "darkMode" : "lightMode")
 });
