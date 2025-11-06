ec850b10d71619bbd38535a3a3c46c75173b771a
Paulo Silva <paulofernando78@gmail.com>
2025-11-06 20:44:14 -0300

    wip

--- src/js/router.js
+++ src/js/router.js
@@ -9,14 +9,17 @@ export async function renderRoute() {
 
   switch (path) {
     case "/":
+      app.innerHTML = "";
       const homeModule = await import("/src/pages/home.js");
       homeModule.Home(app);
       break;
     case "/bikes":
+      app.innerHTML = "";
       const bikesModule = await import("/src/pages/bikes.js");
       bikesModule.Bikes(app);
       break;
     default:
+      app.innerHTML = "";
       const notFoundModule = await import("/src/pages/404.js");
       notFoundModule.NotFound(app);
   }
--- src/pages/bikes.js
+++ src/pages/bikes.js
@@ -1,6 +1,5 @@
 export function Bikes(app) {
-  app.innerHTML = "";
-
+  
   const carouselData = [
     {
       img: "/images/bikes/rider-a1.png",
@@ -28,65 +27,50 @@ export function Bikes(app) {
     },
   ];
 
-  const section = document.createElement("section");
-  section.className = "line-break";
-  app.appendChild(section);
+  app.innerHTML = `
+    <section class="line-break">
+      <h1>Our bikes</h1>
+        <p>Check out our cutting-edge bikes.</p>
+        <div class="wrapper">
+          <ul class="carousel">
+            ${carouselData
+              .map(
+                (bike) => `
+              <li class="carousel__card">
+                <figure>
+                  <div class="carousel__img-wrapper">
+                    <img src="${bike.img}" alt="${bike.alt}" class="bike-img"/>
+                    <button class="carousel__button--left">❮</button>
+                    <button class="carousel__button--right">❯</button>
+                  </div>
+                  <figcaption>
+                    <h2>${bike.title}</h2>
+                    <h3>${bike.subtitle}</h3>
+                    <p>${bike.description}</p>
+                  </figcaption>
+                </figure> 
+              </li>
+              `
+              )
+              .join("")}  
+          </ul>
+        </div>
+    </section>
+      `;
 
-  const h1 = document.createElement("h1");
-  h1.textContent = "Our bikes";
-  section.appendChild(h1);
+  const ul = app.querySelector("ul"); // Needed to link to *ul.scrollBy*
+  const leftBtns = app.querySelectorAll(".carousel__button--left");
+  const rightBtns = app.querySelectorAll(".carousel__button--right");
 
-  const p = document.createElement("p");
-  p.textContent = "Check out our cutting-edge bikes.";
-  section.appendChild(p);
-
-  const wrapper = document.createElement("div");
-  wrapper.className = "wrapper";
-
-  const ul = document.createElement("ul");
-  ul.className = "carousel";
-
-  carouselData.forEach((bike) => {
-    const li = document.createElement("li");
-    li.className = "card";
-    /* html*/
-    li.innerHTML = `
-    <figure>
-      <div class="img-wrapper">
-        <img src="${bike.img}" alt="${bike.alt}" class="bike-img"/>
-        <button class="left-button">❮</button>
-        <button class="right-button">❯</button>
-      </div>
-      <figcaption>
-        <h2>${bike.title}</h2>
-        <h3>${bike.subtitle}</h3>
-        <p>${bike.description}</p>
-    </figcaption>
-      
-    </figure>
-      
-      
-    `;
-    ul.appendChild(li);
+  leftBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      ul.scrollBy({ left: -ul.offsetWidth, behavior: "smooth" });
+    });
   });
 
-  wrapper.append(ul);
-  section.appendChild(wrapper);
-
-  if (ul) {
-    const leftBtn = document.querySelectorAll(".left-button");
-    const rightBtn = document.querySelectorAll(".right-button");
-
-    leftBtn.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        ul.scrollBy({ left: -ul.offsetWidth, behavior: "smooth" });
-      });
+  rightBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      ul.scrollBy({ left: ul.offsetWidth, behavior: "smooth" });
     });
-
-    rightBtn.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        ul.scrollBy({ left: ul.offsetWidth, behavior: "smooth" });
-      });
-    });
-  }
+  });
 }
