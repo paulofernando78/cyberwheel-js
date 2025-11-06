548c8b251c930dbb0f24ccb51f4cc1cf1f30b2d6
Paulo Silva <paulofernando78@gmail.com>
2025-11-06 11:30:44 -0300

    wip

--- src/pages/bikes.js
+++ src/pages/bikes.js
@@ -1,6 +1,5 @@
 export function Bikes(app) {
-
-  app.innerHTML = ""
+  app.innerHTML = "";
 
   const carouselData = [
     {
@@ -30,29 +29,19 @@ export function Bikes(app) {
   ];
 
   const section = document.createElement("section");
-  section.className = "line-break"
+  section.className = "line-break";
   app.appendChild(section);
 
   const h1 = document.createElement("h1");
-  h1.textContent = "Our bikes"
+  h1.textContent = "Our bikes";
   section.appendChild(h1);
 
   const p = document.createElement("p");
-  p.textContent = "Check out our cutting-edge bikes."
+  p.textContent = "Check out our cutting-edge bikes.";
   section.appendChild(p);
 
   const wrapper = document.createElement("div");
   wrapper.className = "wrapper";
-
-  const leftBtn = document.createElement("button");
-  leftBtn.id = "left";
-  leftBtn.className = "carousel-button left-button";
-  leftBtn.textContent = "❮";
-
-  const rightBtn = document.createElement("button");
-  rightBtn.id = "right";
-  rightBtn.className = "carousel-button right-button";
-  rightBtn.textContent = "❯";
 
   const ul = document.createElement("ul");
   ul.className = "carousel";
@@ -62,7 +51,12 @@ export function Bikes(app) {
     li.className = "card";
     /* html*/
     li.innerHTML = `
+    <div class="img-wrapper">
       <img src="${bike.img}" alt="${bike.alt}" class="bike-img"/>
+      <button class="left-button">❮</button>
+      <button class="right-button">❯</button>
+    </div>
+      
       <h2>${bike.title}</h2>
       <h3>${bike.subtitle}</h3>
       <p>${bike.description}</p>
@@ -70,15 +64,23 @@ export function Bikes(app) {
     ul.appendChild(li);
   });
 
-  wrapper.append(leftBtn, ul, rightBtn);
+  wrapper.append(ul);
   section.appendChild(wrapper);
 
-  if (leftBtn && rightBtn && ul) {
-    leftBtn.addEventListener("click", () => {
-      ul.scrollBy({ left: -ul.offsetWidth, behavior: "smooth" });
+  if (ul) {
+    const leftBtn = document.querySelectorAll(".left-button");
+    const rightBtn = document.querySelectorAll(".right-button");
+
+    leftBtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        ul.scrollBy({ left: -ul.offsetWidth, behavior: "smooth" });
+      });
     });
-    rightBtn.addEventListener("click", () => {
-      ul.scrollBy({ left: ul.offsetWidth, behavior: "smooth" });
+
+    rightBtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        ul.scrollBy({ left: ul.offsetWidth, behavior: "smooth" });
+      });
     });
   }
 }
