cb7e36da02a0224e0d08c676af0d5fd4fafca044
Paulo Silva <paulofernando78@gmail.com>
2025-11-12 17:21:26 -0300

    wip

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ export function Home(home) {
   home.innerHTML = `
   <div class="line-break">
   <!--  Introduction -->
-  <section class="line-break">
+  <section class="line-break" aria-label="Introduction">
     <h1 class="home__future">The <span id="future">future</span> of biking starts here</h1>
     <div>
       <p>
@@ -17,13 +17,13 @@ export function Home(home) {
   </section>
 
   <!-- Customization -->
-  <section class="home__customization line-break">
+  <section class="home__customization line-break" aria-label="Customization">
     <h1>Customize yours now</h1>
     <img src="/images/shop.png" alt="Bicycle shop" class="shop"/>
   </section>
 
   <!-- Technology -->
-  <section class="home__technology">
+  <section class="home__technology" aria-label="Technology">
     <div class="line-break">
       <h1>Advance technology</h1>
       <p>
@@ -36,7 +36,7 @@ export function Home(home) {
       <div class="line-break">
         <h2>Durability</h2>
         <p>
-          Bikcraft is designed with precision and built for durability, offering
+          Cyber Wheel is designed with precision and built for durability, offering
           a smooth and efficient ride. Engineered for long-distance performance,
           our bikes are powered by advanced battery technology, ensuring
           effortless speed and endurance.
@@ -45,7 +45,7 @@ export function Home(home) {
       <div class="line-break">
         <h2>Tracking</h2>
         <p>
-          We know how precious your Bikcraft is, so we have added trackers and
+          We know how precious your bike is, so we have added trackers and
           anti-theft systems to ensure your peace of mind.
         </p>
       </div>
@@ -53,7 +53,7 @@ export function Home(home) {
   </section>
 
   <!--  Review -->
-  <section class="home__review-container">
+  <section class="home__review-container" aria-label="Review">
     <img src="/images/review-woman.png" alt="A woman riding a bike" />
     <div class="home__review">
       <p class="home__review-text">
@@ -64,8 +64,35 @@ export function Home(home) {
   </section>
 
   <!-- Our Partners -->
-  <section class="home__our-partners">
-    <h1>Our Partners</h1>
+  <section aria-label="Our Partners">
+    <h1 class="home__partners">Our Partners</h1>
+      <div class="home__partners-slider">
+        <ul class="home__partners-slider__group">
+          <li><img src="/images/partners/caravan.svg" alt="Caravan logo"/></li>
+          <li><img src="/images/partners/dogs.svg" alt="Dogs logo" /></li>
+          <li><img src="/images/partners/flexblog.svg" alt="Flexblog logo" /></li>
+          <li><img src="/images/partners/handel.svg" alt="Handel logo" /></li>
+          <li><img src="/images/partners/lescone.svg" alt="Lescone logo" /></li>
+          <li><img src="/images/partners/ranek.svg" alt="Ranek logo" /></li>
+          <li><img src="/images/partners/surfbot.svg" alt="Surfbot logo" /></li>
+          <li><img src="/images/partners/wildbeast.svg" alt="Wildbeast logo" />
+          </li>
+        </ul>
+
+        <ul class="home__partners-slider__group" aria-hidden="true">
+          <li><img src="/images/partners/caravan.svg" alt="Caravan logo"/></li>
+          <li><img src="/images/partners/dogs.svg" alt="Dogs logo" /></li>
+          <li><img src="/images/partners/flexblog.svg" alt="Flexblog logo" /></li>
+          <li><img src="/images/partners/handel.svg" alt="Handel logo" /></li>
+          <li><img src="/images/partners/lescone.svg" alt="Lescone logo" /></li>
+          <li><img src="/images/partners/ranek.svg" alt="Ranek logo" /></li>
+          <li><img src="/images/partners/surfbot.svg" alt="Surfbot logo" /></li>
+          <li><img src="/images/partners/wildbeast.svg" alt="Wildbeast logo" />
+          </li>
+        </ul>
+        
+      </div>
+      
   </section>
 </div>
   `;
