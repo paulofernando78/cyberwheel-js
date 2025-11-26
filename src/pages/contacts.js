a132b3d8d65cdac0d66ef873c4ccc98ec901edd3
Paulo Silva <paulofernando78@gmail.com>
2025-11-26 20:49:17 -0300

    wip

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -31,7 +31,16 @@ export function Contacts(app) {
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d467690.1437761213!2d-46.924943519041115!3d-23.682063582468334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce448183a461d1%3A0x9ba94b08ff335bae!2sS%C3%A3o%20Paulo%2C%20State%20of%20S%C3%A3o%20Paulo!5e0!3m2!1sen!2sbr!4v1764195365232!5m2!1sen!2sbr" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           <div class="contacts__city line-break">
             <h3 class="display-block">São Paulo</h3>
-
+            <div class="contacts__address">
+              <div class="contacts__address__items">
+                <span>1234 Faria Lima Av.</span>
+                <span>São Paulo - SP</span>
+              </div>
+              <div class="contacts__address__items">
+                <span>sp@cyberwheel.com</span>
+                <span>+55 11 91234-5678</span>
+              </div>
+            </div>
             <div class="contacts__time">
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#999999"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
               <span>8:00AM to 5:00PM</span>
